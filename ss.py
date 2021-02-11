c4041dae323da56820700d9ae4ce1854aa154ba9
jacobhofstein <71907992+jacobhofstein@users.noreply.github.com>
2021-02-11 11:51:32 -0500

    Add files via upload

--- ss.py
+++ ss.py
@@ -5,6 +5,9 @@ import re
 # For now, its inputs are taken as arguments to the function.
 def tokenize(quote, textFileAsString):
 
+    # Set to true while processing an ellipsis.
+    ellipsisFlag = 0
+
     # This tokenizes the text file based on punctuation,
     # but leaves the punctuation as their own tokens.
     tokens = re.split('([.?!])', textFileAsString)
@@ -20,12 +23,49 @@ def tokenize(quote, textFileAsString):
             if tokens[i] == '?' or tokens[i] == '!':
                 tokens[i - 1] += tokens[i]
                 tokens.pop(i)
+                continue
 
-        # We need to detect if the period ended a sentence before knowing what to append.
+        # We need to detect if the period ends a sentence before knowing what to append.
         if i < len(tokens):
             if tokens[i] == '.':
-                # Implement period detection algorithm here
-                placeholder = 0
+
+                # Handles ellipses.
+                if tokens[i + 1] == '.':
+                    ellipsisFlag = 1
+                    tokens[i - 1] += tokens[i]
+                    tokens.pop(i)
+                    continue
+                if ellipsisFlag = 1:
+                    tokens[i - 1] += tokens[i]
+                    tokens.pop(i)
+                    ellipsisFlag = 0
+                    continue
+
+                # Handles acronyms and decimal numbers.
+                if tokens[i + 1][0].isalnum():
+                    tokens[i] += tokens[i + 1]
+                    tokens[i - 1] += tokens[i]
+                    tokens.pop(i + 1)
+                    tokens.pop(i)
+                    continue
+            
+                # Handles abbreviations.
+                prevToken = tokens[i - 1].split(" ")[-1]
+                if prevToken.matches(abbreviationList):
+                    if tokens[i + 1][1].isUpper():
+                        tokens[i - 1] += tokens[i]
+                        tokens.pop(i)
+                        continue
+                    else
+                        tokens[i] += tokens[i + 1]
+                        tokens[i - 1] += tokens[i]
+                        tokens.pop(i + 1)
+                        tokens.pop(i)
+                        continue
+                else
+                    tokens[i - 1] += tokens[i]
+                    tokens.pop(i)
+                    continue
 
         i += 1
 
