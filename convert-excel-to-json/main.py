c99a9f32b231ceae9ff150021aef84020838d3dd
Brent Pappas <pappasbrent@gmail.com>
2021-02-14 10:30:06 -0500

    TODOs and meta dunder vars

--- convert-excel-to-json/main.py
+++ convert-excel-to-json/main.py
@@ -1,6 +1,15 @@
 '''main.py
 Program entry point
+
+TODO:   The code between this component and the algorithm is not DRY,
+        e.g., config.py is copied from that component. This is not amenable
+        to future changes, so some form of unifying these two components
+        should be implemented. Maybe move this a sub-package of the algorithm
+        component?
 '''
+
+__author__ = "Brent Pappas"
+__email__ = "pappasbrent@knights.ucf.edu"
 
 import json
 
@@ -17,7 +26,11 @@ from excel_to_json import write_to_json
 
 @begin.start(auto_convert=True)
 def main(excel_to_json=False, json_to_sql=False):
-    '''Entry point for the program'''
+    '''
+    Entry point for the program
+    TODO: Allow user to specify excel and json filenames from command line
+    TODO: Allow user to toggle ssh tunnelling (see algorithm component)
+    '''
     dotenv.load_dotenv(".dotenv", override=True)
     quotes: dict = None
 
