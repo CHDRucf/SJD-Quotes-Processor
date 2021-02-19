879dff59e65c4908244256b7b21ac433be59012b
Brent Pappas <pappasbrent@gmail.com>
2021-02-19 17:55:27 -0500

    Update to work with new database schema

--- algorithm/util/database_ops.py
+++ algorithm/util/database_ops.py
@@ -3,42 +3,42 @@
 Functions that interact with the database
 '''
 
-from typing import Iterable, List
+from typing import List
 
 from mysql.connector.cursor import CursorBase
 
-from util.custom_types import Metadata, Quote, QuoteMatch
+from util.custom_types import Quote, QuoteMatch, WorkMetadata
 
 
-def write_matches_to_database(matches: Iterable[QuoteMatch], cursor: CursorBase) -> None:
+def write_matches_to_database(matches: List[QuoteMatch], cursor: CursorBase) -> None:
     '''
     Args:
-        matches:    An iterable of quote matches to write to the database
+        matches:    An list of quote matches to write to the database
         conn:       The MySQLConnection object representing a connection to the
                     database
     '''
     sql_insert_statement = (
-        "INSERT INTO `matches`(`quote_id`, `metadata_id`, `rank`, `score`, `content`) "
+        "INSERT INTO `matches`(`quote_id`, `work_metadata_id`, `rank`, `score`, `content`) "
         "VALUES (%s, %s, %s, %s, %s);"
     )
     cursor.executemany(sql_insert_statement, matches)
 
 
-def get_metadatas(cursor: CursorBase) -> Metadata:
+def get_works_metadata(cursor: CursorBase) -> List[WorkMetadata]:
     '''
-    Gets all the metadatas from the MySQL database
-    TODO: Add filters to only search over specific metadatas
+    Gets all the metadata for all the works from the MySQL database
+    TODO: Add filters to only search over specific metadata
 
     Args:
         cursor: The database cursor for performing the metadata query
 
-    Returns:    A list of the Metadata objects representing the metadatas found
+    Returns:    A list of the Metadata objects representing the metadata found
     '''
     sql_query: str = (
-        "SELECT id, title, author, url, filepath, lccn "
-        "FROM metadata;")
+        "SELECT `id`, `title`, `author`, `url`, `filepath`, `lccn` "
+        "FROM `work_metadata`;")
     cursor.execute(sql_query)
-    return [Metadata(*row) for row in cursor.fetchall()]
+    return [WorkMetadata(*row) for row in cursor.fetchall()]
 
 
 def get_quotes(cursor: CursorBase) -> List[Quote]:
@@ -52,8 +52,8 @@ def get_quotes(cursor: CursorBase) -> List[Quote]:
     Returns:    A list of Quote objects representing the quotes found
     '''
     select_quotes_sql = (
-        "SELECT id, headword, quote, title, author "
-        "FROM quotes;"
+        "SELECT `id`, `content` "
+        "FROM `quotes`;"
     )
     cursor.execute(select_quotes_sql)
     return [Quote(*row) for row in cursor.fetchall()]
