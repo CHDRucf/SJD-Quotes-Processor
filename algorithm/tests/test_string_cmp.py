9e48de6441eb72ffac8e4d8f4e858b9eb1c768f5
Brent Pappas <pappasbrent@gmail.com>
2021-02-20 09:42:21 -0500

    Add basic setence comparison tests

--- algorithm/tests/test_string_cmp.py
+++ algorithm/tests/test_string_cmp.py
@@ -5,7 +5,7 @@ Tests for the string_cmp module
 
 from typing import Any, List, Set
 
-from util.string_comp import jaccard_index
+from util.string_comp import compare_quote_to_sentence, jaccard_index
 
 
 def test_jaccard_index():
@@ -17,3 +17,15 @@ def test_jaccard_index():
     ]
     for set1, set2, expected in sets_and_j_values:
         assert jaccard_index(set1, set2) == expected
+
+
+def test_compare_quote_to_sentence():
+    for quote, sentence in [
+        ("A hunting Chloë went.",
+         "a hunting Chloe went"),
+        ("They go a begging to a bankrupt’s door.",
+         "they a go a beggin' to a bankrupts door"),
+        ("May pure contents for ever pitch their tents Upon these downs, these meads, these rocks, these mountains, And peace still slumber by these purling fountains! Which we may every year Find when we come a fishing here.",
+         "May pvre contents forever pitch there tents upon these downs, these meadows, these rocks, these mountains, And peece still slumber by these stirling fountains! which we may evr'y year find when we come a fishin' here."),
+    ]:
+        assert compare_quote_to_sentence(quote, sentence) > 0.9
