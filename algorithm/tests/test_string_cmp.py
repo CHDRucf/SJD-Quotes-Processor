496eb486d849abd866f19260f26451fcdd07ce7a
Brent Pappas <pappasbrent@gmail.com>
2021-02-20 11:10:24 -0500

    Qoute to sentence compare tests

--- algorithm/tests/test_string_cmp.py
+++ algorithm/tests/test_string_cmp.py
@@ -5,6 +5,7 @@ Tests for the string_cmp module
 
 from typing import Any, List, Set
 
+import pytest
 from util.string_comp import compare_quote_to_sentence, jaccard_index
 
 
@@ -19,13 +20,58 @@ def test_jaccard_index():
         assert jaccard_index(set1, set2) == expected
 
 
-def test_compare_quote_to_sentence():
-    for quote, sentence in [
-        ("A hunting Chloë went.",
-         "a hunting Chloe went"),
-        ("They go a begging to a bankrupt’s door.",
-         "they a go a beggin' to a bankrupts door"),
-        ("May pure contents for ever pitch their tents Upon these downs, these meads, these rocks, these mountains, And peace still slumber by these purling fountains! Which we may every year Find when we come a fishing here.",
-         "May pvre contents forever pitch there tents upon these downs, these meadows, these rocks, these mountains, And peece still slumber by these stirling fountains! which we may evr'y year find when we come a fishin' here."),
-    ]:
-        assert compare_quote_to_sentence(quote, sentence) > 0.9
+@pytest.mark.parametrize(
+    "quote, sentence, work, author, url",
+    [
+        (
+            "That is, every thing is the better, the same, the fitter.  Sceptre and pow’r, "
+            "thy giving, I assume; And glad her shall resign, when in the end Thou shalt be all "
+            "in all, and I in thee, For ever; and in me all whom thou lov’st.",
+
+            '''Scepter and Power, thy giving, I assume,
+And gladlier shall resign, when in the end
+Thou shalt be All in All, and I in thee
+For ever, and in mee all whom thou lov’st:
+But whom thou hat’st, I hate, and can put on
+Thy terrors, as I put thy mildness on,
+Image of thee in all things
+and shall soon,
+Armd with thy might, rid heav’n of these rebell’d,
+To thir prepar’d ill Mansion driven down
+To chains of Darkness, and th’ undying Worm,
+That from thy just obedience could revolt,
+Whom to obey is happiness entire.''',
+
+            "Paradise Lost", "John Milton", "http://www.gutenberg.org/files/20/20-0.txt"
+        ),
+        (
+            "Up with my tent, here will I lie to night; But where to morrow? —— Well, all’s one for that.",
+
+            '''Up With my tent! Here will I lie to-night;
+                      [Soldiers begin to set up the KING'S tent]
+    But where to-morrow?''',
+
+            "Richard III", "William Shakespeare", "http://www.gutenberg.org/cache/epub/1103/pg1103.txt"
+        ),
+        (
+            "They that do not keep up this indifferency for all but truth, put coloured spectacles before their eyes, and look through false glasses.",
+
+            "They that do not keep up this indifferency in themselves for all but truth, not supposed, but evidenced in themselves, put coloured spectacles before their eyes, and look on things through false glasses, and then think themselves excused in following the false appearances, which they themselves put upon them.",
+
+            "The Works, vol. 2 An Essay concerning Human Understanding Part 2 and Other Writings", "John Locke", "https://oll.libertyfund.org/title/locke-the-works-vol-2-an-essay-concerning-human-understanding-part-2-and-other-writings"
+        ),
+        (
+            "All the fitter, Lentulus: our coming Is not for salutation; we have bus’ness.",
+
+            '''Why, all the fitter, LENTVLVS: our comming 200
+195
+189 you
+185[Noise within.''',
+
+            "Ben Jonson", "Catiline his conspiracy", "https://catalog.hathitrust.org/Record/001017535"
+        )
+    ])
+def test_compare_quote_to_sentence(quote: str, sentence: str, work: str, author: str, url: str):
+    TARGET_THRESHOLD = 0.9
+    assert compare_quote_to_sentence(
+        quote, sentence) > TARGET_THRESHOLD, f"Info:\nWork:   {work}\nAuthor: {author}\nURL:    {url}"
