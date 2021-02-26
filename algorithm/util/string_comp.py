ca74401869923a1d566622b040e14007cc18edbd
Brent Pappas <pappasbrent@gmail.com>
2021-02-25 21:05:16 -0500

    Add sentence construction feature

--- algorithm/util/string_comp.py
+++ algorithm/util/string_comp.py
@@ -6,125 +6,13 @@ metrics
 
 import operator
 import os
-import re
-import string
-from typing import List, Set
+from collections import deque
+from typing import Deque, List
+import logging
 
-import Levenshtein
+import rapidfuzz
 
 from util.custom_types import Quote, QuoteMatch, WorkMetadata
-from util.misc import weighted_average
-
-
-def split_by_punctuation(text: str) -> List[str]:
-    tokens = re.split('([.?!;,:])', text)
-    
-    if tokens[len(tokens) - 1] == '':
-        tokens.pop()
-       
-    i = 1
-    while i < len(tokens):
-        if tokens[i] == '?' or tokens[i] == '!':
-            tokens[i - 1] += tokens[i]
-            tokens.pop(i)
-            if i == len(tokens):
-                break
-            else:
-                continue
-        if tokens[i] == '.':
-            if i < len(tokens) - 1:
-                if tokens[i + 1] == '.':
-                    tokens[i - 1] += tokens[i]
-                    tokens.pop(i)
-                    tokens[i - 1] += tokens[i]
-                    tokens.pop(i)
-                    continue
-            if i < len(tokens) - 1 and len(tokens[i + 1]) > 1:
-                if tokens[i + 1][0].isalnum():
-                    tokens[i] += tokens[i + 1]
-                    tokens[i - 1] += tokens[i]
-                    tokens.pop(i + 1)
-                    tokens.pop(i)
-                    continue
-            if i < len(tokens) - 1 and len(tokens[i + 1]) > 1:
-                if tokens[i + 1][1].isupper():
-                    tokens[i - 1] += tokens[i]
-                    tokens.pop(i)
-                    continue
-            if (i < len(tokens) - 1) and len(tokens[i + 1]) > 1:
-                if tokens[i + 1][1].islower():
-                    tokens[i] += tokens[i + 1]
-                    tokens[i - 1] += tokens[i]
-                    tokens.pop(i + 1)
-                    tokens.pop(i)
-                    continue
-                
-            tokens[i - 1] += tokens[i]
-            tokens.pop(i)
-            if (i == len(tokens)):
-                break
-                
-        if tokens[i] == ";":
-            tokens[i - 1] += tokens[i]
-            tokens.pop(i)
-            continue
-        if tokens[i] == ",":
-            tokens[i - 1] += tokens[i]
-            tokens.pop(i)
-            continue
-        if tokens[i] == ":":
-            tokens[i - 1] += tokens[i]
-            tokens.pop(i)
-            continue
-            
-        i += 1
-            
-    # return re.split(r"[.!?]+", text)
-    return tokens
-
-
-def jaccard_index(set1: set, set2: set) -> float:
-    ''' Computes the Jaccard Index between two sets'''
-    return len(set1 & set2) / len(set1 | set2)
-
-
-def compare_quote_to_sentence(quote: str, sentence: str) -> float:
-    '''
-    Compares a quote to a sentence and returns a normalized scalar value
-    representing how similar they are. The two strings should all be in
-    the matching case and have all punctuation removed beforehand
-
-    Args:
-        quote:      The quote to compare against the target sentence.
-        sentence:   The sentence to be compared against
-
-    Returns:
-        result: The normalized scalar value representing how similar the
-        quote is to the sentence. The closer to 1 this value is , the more
-        similar the the quote and sentence are
-    '''
-    J_WEIGHT = 0.25
-    L_WEIGHT = 0.75
-
-    punc_replace_pattern = re.compile(re.escape(string.punctuation))
-
-    # normalize quote string
-    quote = quote.lower()
-    quote = re.sub(punc_replace_pattern, "", quote)
-
-    # normalize sentence string
-    sentence = sentence.lower()
-    sentence = re.sub(punc_replace_pattern, "", sentence)
-
-    quote_set: Set[str] = set(quote)
-    sentence_set: Set[str] = set(sentence)
-    j_index: float = jaccard_index(quote_set, sentence_set)
-
-    edit_distance: float = Levenshtein.ratio(quote, sentence)
-
-    result = weighted_average([(j_index, J_WEIGHT), (edit_distance, L_WEIGHT)])
-
-    return result
 
 
 def fuzzy_search_over_file(quote: Quote, work_metadata: WorkMetadata, text_file_string: str) -> List[QuoteMatch]:
@@ -140,50 +28,33 @@ def fuzzy_search_over_file(quote: Quote, work_metadata: WorkMetadata, text_file_
     Returns:    A list of the top five matches found
     '''
 
-    # TODO: Implement this function
-    sentences: List[str] = split_by_punctuation(text_file_string)
+    window_size: int = len(quote.content)
+    window_slide: int = window_size // 2
+    sentences = [
+        text_file_string[i:i+window_size]
+        for i in range(0, len(text_file_string) - len(quote.content)//2, window_slide)
+    ]
 
-    # TODO: Implement logic to incrementally increase size of quote
-    # for quotes with multiple sentences
+    matches = rapidfuzz.process.extract(
+        quote.content, sentences, scorer=rapidfuzz.fuzz.ratio)
 
-    # Get all matches
-    """ matches = [
-        QuoteMatch(
-            quote.id, work_metadata.id, 0,
-            compare_quote_to_sentence(quote.content, s), s)
-        for s in sentences
-    ] """
-    
-    matches = []
-    
-    i = 0
-    while i < len(sentences):
-        res = compare_quote_to_sentence(quote.content, sentences[i])
-        
-        j = 1
-        curr = sentences[i]
-        final = ""
-        while (i + j) < len(sentences):
-            curr += sentences[i + j]
-            final += sentences[i + j - 1]
-            
-            new_res = compare_quote_to_sentence(quote.content, curr)
-            
-            if (new_res > res) and new_res >= 0.5:
-                res = new_res
-            else:
-                break
-            
-            j += 1
-        
-        matches.append(QuoteMatch(quote.id, work_metadata.id, 0, res, final))
-        
-        i += 1
-        
-        
+    results: Deque[QuoteMatch] = deque()
+    # Expands the quote to be a sentence instead of an arbitrary substring
+    for _, score, index in matches:
+        start: int = index * window_slide
+        end: int = start + window_size
+        end_punc: List[str] = ['.', '?', '!', ':']
+        while start > 0 and text_file_string[start] not in end_punc:
+            start -= 1
+        while end < len(text_file_string and text_file_string[end]) not in end_punc:
+            end += 1
 
-    # Return top five matches found
-    return list(sorted(matches, key=operator.attrgetter("score"), reverse=True)[:5])
+        sentence = text_file_string[start:end]
+
+        results.append(QuoteMatch(
+            quote.id, work_metadata.id, 0, score, sentence))
+
+    return list(sorted(results, reverse=True)[:5])
 
 
 def fuzzy_search_over_corpora(quote: Quote, work_metadatas: List[WorkMetadata], corpora_path: str) -> List[QuoteMatch]:
@@ -207,17 +78,20 @@ def fuzzy_search_over_corpora(quote: Quote, work_metadatas: List[WorkMetadata], 
     for work_metadata in work_metadatas:
         filepath = os.path.join(corpora_path, work_metadata.filepath)
 
-        with open(filepath, "r", encoding="utf-8") as fp:
-            text_file_string: str = fp.read()
+        try:
+            with open(filepath, "r", encoding="utf-8") as fp:
+                text_file_string: str = fp.read()
 
-        top_five_matches_in_file: List[QuoteMatch] = fuzzy_search_over_file(
-            quote, work_metadata, text_file_string)
+            top_five_matches_in_file: List[QuoteMatch] = fuzzy_search_over_file(
+                quote, work_metadata, text_file_string)
 
-
-        top_five_overall = list(
-            sorted(
-                top_five_matches_in_file + top_five_overall, key=operator.attrgetter("score"),
-                reverse=True
-            )[:5])
+            top_five_overall = list(
+                sorted(
+                    top_five_matches_in_file + top_five_overall, key=operator.attrgetter("score"),
+                    reverse=True
+                )[:5])
+        except FileNotFoundError:
+            logging.warning(
+                f"File at path '{filepath}' not found; searching skipped")
 
     return top_five_overall
