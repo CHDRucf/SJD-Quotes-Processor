1bcee844047355df522d03dd959852299fc6c214
Vice <drewschilling09@gmail.com>
2021-03-18 17:36:29 -0400

    Added php endpoints for the frontend to indirectly call the API

--- frontend/php/get_matches_by_title.php
+++ frontend/php/get_matches_by_title.php
@@ -1,12 +1,12 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $post = [
-	    'headword' => $_POST['searchBox'],
+	    'title' => $_POST['searchBox'],
     ];
   }
 
 $curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, 'http://localhost:5000/get_matches_by_headword');
+curl_setopt($curl, CURLOPT_URL, 'http://localhost:5000/get_matches_by_title');
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($post));
 curl_setopt($curl, CURLOPT_POST, true);
