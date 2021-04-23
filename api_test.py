4bd7d67e80e875bd549e2ef40bbd90258d38891a
DataScienceAndMemes <79790650+DataScienceAndMemes@users.noreply.github.com>
2021-04-23 11:28:05 -0400

    Add files via upload

--- api_test.py
+++ api_test.py
@@ -11,73 +11,28 @@ class FlaskTest(unittest.TestCase):
         
     def tearDown(self):
         pass
-    
-    def test_login(self):   
-        response = self.application.get('/login', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-    
-    def test_login_content(self):   
-        response = self.application.get('/login', follow_redirects=True)
-        self.assertEqual(response.content_type, 'application/json')  
-    
-    def test_login_success(self):   
-        response = self.application.post('/login', data=dict(
-            username='silveresque',
-            password='password123'
-            ), follow_redirects=True)
-        
-        data = response.data
-        data_string = data.decode('utf-8')
-        data_dict = json.loads(data_string)
-        self.assertEqual(data_dict['message'], 'success')
-        
-    def test_login_failure1(self):   
-        response = self.application.post('/login', data=dict(
-            username='silveresques',
-            password='password123'
-            ), follow_redirects=True)
-        
-        data = response.data
-        data_string = data.decode('utf-8')
-        data_dict = json.loads(data_string)
-        self.assertEqual(data_dict['message'], 'username or password is invalid')
-        
-    def test_login_failure2(self):   
-        response = self.application.post('/login', data=dict(
-            username='silveresque',
-            password='password132'
-            ), follow_redirects=True)
-        
-        data = response.data
-        data_string = data.decode('utf-8')
-        data_dict = json.loads(data_string)
-        self.assertEqual(data_dict['message'], 'username or password is invalid')
         
     def test_matches_by_title(self):   
         response = self.application.post('/get_matches_by_title', data=dict(
-            title='something'
+            title='something',
+            condition='all',
+            textFormat='contains'
             ), follow_redirects=True)
         self.assertEqual(response.status_code, 200)
     
     def test_matches_by_title_content(self):   
         response = self.application.post('/get_matches_by_title', data=dict(
-            title='something'
+            title='something',
+            condition='all',
+            textFormat='contains'
             ), follow_redirects=True)
         self.assertEqual(response.content_type, 'application/json') 
         
-    def test_matches_by_title_tobit(self):
-        response = self.application.post('/get_matches_by_title', data=dict(
-            title='tobit'
-            ), follow_redirects=True)
-        
-        data = response.data
-        data_string = data.decode('utf-8')
-        data_dict = json.loads(data_string)
-        self.assertEqual(len(data_dict), 2)
-        
     def test_matches_by_title_on_medals(self):
         response = self.application.post('/get_matches_by_title', data=dict(
-            title='on medals'
+            title='on medals',
+            condition='all',
+            textFormat='contains'
             ), follow_redirects=True)
         
         data = response.data
@@ -87,7 +42,9 @@ class FlaskTest(unittest.TestCase):
         
     def test_matches_by_title_garbage_input(self):
         response = self.application.post('/get_matches_by_title', data=dict(
-            title='asdf;lajsdf98qefj1opil4j2139f81pjuf12f'
+            title='asdf;lajsdf98qefj1opil4j2139f81pjuf12f',
+            condition='all',
+            textFormat='contains'
             ), follow_redirects=True)
         
         data = response.data
@@ -95,22 +52,105 @@ class FlaskTest(unittest.TestCase):
         data_dict = json.loads(data_string)
         self.assertEqual(len(data_dict), 0)
         
+    def test_matches_by_headword(self):   
+        response = self.application.post('/get_matches_by_headword', data=dict(
+            headword='the',
+            condition='all',
+            textFormat='contains'
+            ), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)    
+        
+    def test_matches_by_headword_content(self):   
+        response = self.application.post('/get_matches_by_headword', data=dict(
+            headword='the',
+            condition='all',
+            textFormat='contains'
+            ), follow_redirects=True)
+        self.assertEqual(response.content_type, 'application/json') 
+        
+    def test_matches_by_headword_contains(self):   
+        response = self.application.post('/get_matches_by_headword', data=dict(
+            headword='the',
+            condition='all',
+            textFormat='contains'
+            ), follow_redirects=True)
+        
+        data = response.data
+        data_string = data.decode('utf-8')
+        data_dict = json.loads(data_string)
+        self.assertEqual(len(data_dict), 1856)  
+        
+    def test_matches_by_headword_exact(self):   
+        response = self.application.post('/get_matches_by_headword', data=dict(
+            headword='the',
+            condition='all',
+            textFormat='exact'
+            ), follow_redirects=True)
+        
+        data = response.data
+        data_string = data.decode('utf-8')
+        data_dict = json.loads(data_string)
+        self.assertEqual(len(data_dict), 23) 
+        
+    def test_matches_by_headword_startswith(self):   
+        response = self.application.post('/get_matches_by_headword', data=dict(
+            headword='the',
+            condition='all',
+            textFormat='startswith'
+            ), follow_redirects=True)
+        
+        data = response.data
+        data_string = data.decode('utf-8')
+        data_dict = json.loads(data_string)
+        self.assertEqual(len(data_dict), 381)  
+        
+    def test_matches_by_author(self):   
+        response = self.application.post('/get_matches_by_author', data=dict(
+            author='shakespeare',
+            condition='all',
+            textFormat='contains'
+            ), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)  
+        
+    def test_matches_by_author_content(self):   
+        response = self.application.post('/get_matches_by_author', data=dict(
+            author='shakespeare',
+            condition='all',
+            textFormat='contains'
+            ), follow_redirects=True)
+        self.assertEqual(response.content_type, 'application/json') 
+        
+    def test_matches_by_author_shakespeare(self):
+        response = self.application.post('/get_matches_by_author', data=dict(
+            author='shakespeare',
+            condition='all',
+            textFormat='contains'
+            ), follow_redirects=True)
+        
+        data = response.data
+        data_string = data.decode('utf-8')
+        data_dict = json.loads(data_string)
+        self.assertEqual(len(data_dict), 15541)
+        
     def test_matches_by_random(self):   
         response = self.application.post('/get_matches_by_random', data=dict(
-            number=1
+            number=1,
+            condition='all'
             ), follow_redirects=True)
         self.assertEqual(response.status_code, 200)
     
     
     def test_matches_by_random_content(self):   
         response = self.application.post('/get_matches_by_random', data=dict(
-            number=1
+            number=1,
+            condition='all'
             ), follow_redirects=True)
         self.assertEqual(response.content_type, 'application/json') 
     
     def test_matches_by_random_10(self):
         response = self.application.post('/get_matches_by_random', data=dict(
-            number=10
+            number=10,
+            condition='all'
             ), follow_redirects=True)
         
         data = response.data
@@ -120,7 +160,8 @@ class FlaskTest(unittest.TestCase):
     
     def test_matches_by_random_25(self):
         response = self.application.post('/get_matches_by_random', data=dict(
-            number=25
+            number=25,
+            condition='all'
             ), follow_redirects=True)
         
         data = response.data
@@ -130,15 +171,14 @@ class FlaskTest(unittest.TestCase):
         
     def test_matches_by_random_100(self):
         response = self.application.post('/get_matches_by_random', data=dict(
-            number=100
+            number=100, 
+            condition='all'
             ), follow_redirects=True)
         
         data = response.data
         data_string = data.decode('utf-8')
         data_dict = json.loads(data_string)
         self.assertEqual(len(data_dict), 100) 
-        
-    
         
 if __name__ == "__main__":
     unittest.main()
